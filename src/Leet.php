912f4b2a63ffc94e91065832b5bd22678eecdf62
miyata <rm.eilsystem@gmail.com>
2021-09-18 16:42:16 +0900

    big bang

---
+++ src/Leet.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Leet;
+
+class Leet
+{
+    /**
+     * 変換文字列の定義
+     *
+     * @var array
+     */
+    public const LEET_STRINGS = [
+        'a' => 4,
+        'b' => 8,
+        'd' => 17,
+        'e' => 3,
+        'g' => 6,
+        'i' => 1,
+        'l' => 1,
+        'o' => 0,
+        'p' => 9,
+        'r' => 2,
+        's' => 5,
+        't' => 7,
+        'y' => 7,
+        'z' => 2,
+    ];
+
+    /**
+     * 変換
+     *
+     * @param string $str 変換前の文字列
+     *
+     * @return string
+     */
+    public static function generate($str = '')
+    {
+        $generated = '';
+        if ($str === '') {
+            return $generated;
+        }
+
+        $lower = strtolower($str);
+        foreach (str_split($lower) as $value) {
+            $generated .= self::LEET_STRINGS[$value] ?? $value;
+        }
+
+        return $generated;
+    }
+}
---
+++ tests/LeetTest.php
@@ -0,0 +1,17 @@
+<?php
+
+use Leet\Leet;
+use PHPUnit\Framework\TestCase;
+
+class LeetTest extends TestCase
+{
+    public function test空文字の時に空文字が返る()
+    {
+        $this->assertSame('', Leet::generate(''));
+    }
+
+    public function testLeet文字に変換される()
+    {
+        $this->assertSame('48c173f6h1jk1mn09q257uvwx72', Leet::generate('AbCdefGhiJkLmNOPQrStUVwxyZ'));
+    }
+}
